4bd6614cfc3f228cffe18f80b2bf6b808509dd35
dima <mail.dima.msk@ya.ru>
2021-04-20 12:10:18 +0300

    Добавьте файлы проекта.

---
+++ Common.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+
+namespace CheckService
+{
+    public class Sender
+    {
+        public string User { get; set; }
+        public string Server { get; set; }
+        public string Login { get; set; }
+        public string Pass { get; set; }
+        public string Subject { get; set; }
+        public int _port { get; set; }
+        public string Port { set { _port = int.Parse(value); } }
+        public string Boby { get; set; }
+  
+    }
+
+    public class STask
+    {
+        public string Name { get; set; } = string.Empty;
+        public string ErrorText { get; set; } = string.Empty;
+        public bool IsError { get; set; } = false;
+        public string ConnectionString { get; set; } = string.Empty;
+        public List<TimeSpan> ScheduleDT = new List<TimeSpan>();
+        private string _sql_query = string.Empty;
+        public string SqlQuery
+        { 
+            get 
+            {
+                return _sql_query; 
+            } 
+            set 
+            {
+                try //запрещаем прямые инструкции на изменение
+                {
+                    var tval = value.ToLower();
+
+                    if (tval.Contains("update "))
+                        throw new Exception();
+                    if (tval.Contains("insert "))
+                        throw new Exception();
+                    if (tval.Contains("delete "))
+                        throw new Exception();
+                    if (tval.Contains("create "))
+                        throw new Exception();
+
+                    _sql_query = value;
+                }
+                catch
+                {
+                    _sql_query = string.Empty;
+                }
+            } 
+        }
+        public string SqlStoredProcedure { get; set; } = string.Empty;
+        public string Schedule { 
+            set 
+            {
+                ScheduleDT = new List<TimeSpan>();
+                string val = value.ToLower();
+                if (val.StartsWith("day"))
+                    ScheduleDT.Add(Common.DefaultSchedule);
+                if (Regex.IsMatch(val, @"\d{1,2}([0-9;])*"))
+                {
+                    foreach (var t in val.Split(';'))
+                    {
+                        try
+                        {
+                            var dt = TimeSpan.FromHours(int.Parse(t));
+                            if (!ScheduleDT.Contains(dt))
+                                ScheduleDT.Add(dt);
+                        }
+                        catch { }
+                    }
+                }
+            } 
+        }        
+    }
+
+    class Common
+    {
+        public static STask[] ActualTasks = new STask[0];
+        public static Sender sender = null;
+        public static string[] recipients = null;
+        public static DateTime ConfigLastChange = new DateTime();
+        public static TimeSpan DefaultSchedule = DateTime.Now.TimeOfDay;        
+        public static string path = AppDomain.CurrentDomain.BaseDirectory + "appsettings.json";
+        public static string syslog = AppDomain.CurrentDomain.BaseDirectory + "SysLog";
+
+        public static void ConfHasChanges()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+       
+
+            FileInfo fi = new FileInfo(path);
+            //if (fi.LastWriteTime > ConfigLastChange)
+            {
+                ActualTasks = new STask[0];
+                using (var file = new StreamReader(path, Encoding.GetEncoding(1251)))
+                {
+                    var jr = new JsonTextReader(file);
+                    var j = JObject.Load(jr);
+                    var tmp_dt = j.GetValue("DefaultSchedule").Value<string>();
+                    if (tmp_dt != null && !string.IsNullOrEmpty(tmp_dt) && int.TryParse(tmp_dt, out int defTime))
+                        DefaultSchedule = TimeSpan.FromHours(defTime);
+
+                    var tmp_sender = j.GetValue("MailConf").ToString();
+                    sender = JsonConvert.DeserializeObject<Sender>(tmp_sender);
+
+                    recipients = JsonConvert.DeserializeObject<string[]>(j.GetValue("MailRecipient").ToString()); 
+
+                    var tmp_data = j.GetValue("Data").ToString();
+                    ActualTasks = JsonConvert.DeserializeObject<STask[]>(tmp_data);                    
+                }
+                ConfigLastChange = fi.LastWriteTime;                
+            }            
+        }
+
+    }
+
+    public class Log
+    {
+        private static object s = new object();
+        public static void Write(Exception e)
+        {
+            Task.Run(() => {
+                lock (s)
+                {
+                    try
+                    {
+                        if (!Directory.Exists(AppDomain.CurrentDomain.BaseDirectory + "ErrorLog"))
+                            Directory.CreateDirectory(AppDomain.CurrentDomain.BaseDirectory + "ErrorLog");
+
+                        using (var sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "ErrorLog\\Log_" + DateTime.Now.ToString("yyyy_MM_dd_HH") + ".log", true))
+                        {
+                            sw.WriteLine("\r\n" + DateTime.Now + "   :   " + e.Message);
+                            if (e.StackTrace != null && !string.IsNullOrEmpty(e.StackTrace))
+                                sw.WriteLine("  " + e.StackTrace);
+                        }
+                    }
+                    catch { }
+                }
+            });
+        }
+        public static void Write(STask t)
+        {
+            Task.Run(() => {
+                lock (s)
+                {
+
+                    if (!Directory.Exists(Common.syslog))
+                        Directory.CreateDirectory(Common.syslog);
+                    else
+                    {
+                        foreach (var f in Directory.GetFiles(Common.syslog))
+                        {
+                            var fi = new FileInfo(f);
+                            if (fi.CreationTime < DateTime.Now.AddDays(-7))
+                                File.Delete(f);
+                        }
+                    }
+
+                    try
+                    {
+                        using (var sw = new StreamWriter(Common.syslog + "\\Log_" + DateTime.Now.ToString("yyyy_MM_dd") + ".log", true))
+                        {
+                            sw.WriteLine("\r\n" + t.Name);
+                            sw.WriteLine(DateTime.Now);
+                        }
+                    }
+                    catch { }
+                }
+            });
+        }
+    }
+}
---
+++ Worker.cs
@@ -0,0 +1,193 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+
+namespace CheckService
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    //собираем все задачи из конф. файла
+                    Common.ConfHasChanges();
+                    if (Common.ActualTasks.Length != 0)
+                    {
+                        
+                        var alreadyWorked = new List<KeyValuePair<string, DateTime>>();
+
+                        if (!Directory.Exists(Common.syslog))
+                            Directory.CreateDirectory(Common.syslog);
+                        else
+                        {
+                            var fname = Common.syslog + "\\Log_" + DateTime.Now.ToString("yyyy_MM_dd") + ".log";
+                            //провер€ем когда какие задачи были выполнены из лог файла
+                            if (File.Exists(fname))
+                            {
+                                var syslog = File.ReadAllLines(fname).Where(m => !string.IsNullOrEmpty(m)).ToArray();
+                                for (int i = 0; i < syslog.Length - 1; i++)
+                                {
+                                    if (Regex.IsMatch(syslog[i], @"[a-zA-Zа-€ј-я0-9]+") && Regex.IsMatch(syslog[i + 1], @"\d{1,2}\.\d{1,2}\.\d{4}\ \d{1,2}\:\d{1,2}\:\d{1,2}"))
+                                        alreadyWorked.Add(new KeyValuePair<string, DateTime>(syslog[i], DateTime.Parse(syslog[i + 1])));
+                                }
+                            }
+                        }
+
+                        List<STask> plannedTasks = new List<STask>();
+                        //ставим на выполнение
+                        foreach (var task in Common.ActualTasks.Where(m=>!string.IsNullOrEmpty(m.Name)))
+                        {
+                            if (task.ScheduleDT.Count == 1)
+                            {
+                                DateTime pdate = DateTime.Now.Date.AddHours(task.ScheduleDT.FirstOrDefault().TotalHours);
+                                if (DateTime.Now > pdate)
+                                {
+                                    var c = alreadyWorked.Where(m => m.Key == task.Name && m.Value.Date == pdate.Date);                                  
+                                    if (c.Count() == 0)
+                                        plannedTasks.Add(task);
+                                }
+                            }
+                            else
+                            {
+                                foreach (var pvar in task.ScheduleDT.OrderBy(m => m)) 
+                                {
+                                    DateTime pdate = DateTime.Now.Date.AddHours(pvar.TotalHours);
+                                  
+                                    if (DateTime.Now.Date == pdate.Date && (int)DateTime.Now.TimeOfDay.TotalHours == (int)pdate.TimeOfDay.TotalHours)
+                                    {
+                                        var c = alreadyWorked.Where(m => m.Key == task.Name && m.Value.Date == pdate.Date && (int)m.Value.TimeOfDay.TotalHours == (int)pdate.TimeOfDay.TotalHours);                                       
+                                        if (c.Count() == 0)
+                                        {
+                                            plannedTasks.Add(task);
+                                            break;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+
+                        if (plannedTasks.Count != 0)
+                        {                            
+                            foreach (STask task in plannedTasks)
+                            {
+                                using (DataTable dt = new DataTable())
+                                {
+                                    try
+                                    {
+                                        using (var sql = new SqlConnection())
+                                        {
+                                            sql.ConnectionString = task.ConnectionString;
+                                            sql.Open();
+                                            using (var cmd = new SqlCommand())
+                                            {
+                                                cmd.Connection = sql;
+                                                cmd.CommandType = string.IsNullOrEmpty(task.SqlStoredProcedure) ? System.Data.CommandType.Text : System.Data.CommandType.StoredProcedure;
+                                                cmd.CommandText = string.IsNullOrEmpty(task.SqlStoredProcedure) ? task.SqlQuery : task.SqlStoredProcedure;
+
+                                                dt.Load(cmd.ExecuteReader());
+
+                                                if (dt.Rows.Count != 0)
+                                                {
+                                                    //фиксируем ошибки дл€ сообщени€
+                                                    string errstr = "";
+                                                    foreach (DataRow row in dt.Rows)
+                                                    {
+                                                        foreach (DataColumn col in dt.Columns)
+                                                        {
+                                                            if(row[col.ColumnName] != DBNull.Value)
+                                                            errstr += "<span>" + row[col.ColumnName].ToString() + "</span> ";
+                                                        }
+                                                        errstr += "<br>";
+                                                    }
+
+                                                    throw new Exception(errstr);
+                                                }
+
+                                             
+                                            }
+                                        }
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        task.IsError = true;
+                                        task.ErrorText = ex.Message;
+                                        Log.Write(ex);
+                                    }
+                                }
+                                //пишем системный лог о работе сервиса дл€ последующего цикла
+                                Log.Write(task);
+                            }
+
+                            var errors = plannedTasks.Where(m => m.IsError);
+                            if (errors.Count() != 0)
+                            {
+                                try
+                                {
+                                    using (MailMessage m = new MailMessage())
+                                    {
+                                        m.From = new MailAddress(Common.sender.Login);                                       
+                                        foreach (var r in Common.recipients)
+                                        {
+                                            if (Regex.IsMatch(r, @".+\@.+\.\w{2,10}"))
+                                                m.To.Add(new MailAddress(r));
+                                        }
+                                        m.Subject = Common.sender.Subject;
+                                        m.IsBodyHtml = true;
+                                        m.Body = "<h3>" + DateTime.Now + "</h3>";
+                                        foreach (var error in errors)
+                                        {
+                                            m.Body += "<p><span>" + error.Name + " : </span>" + error.ErrorText + "</p>";
+                                        }
+                                        m.BodyEncoding = Encoding.UTF8;
+
+                                        using (SmtpClient smtp = new SmtpClient(Common.sender.Server, Common.sender._port))
+                                        {                                            
+                                            smtp.Credentials = new NetworkCredential(Common.sender.Login, Common.sender.Pass);
+                                            smtp.EnableSsl = true;
+                                            smtp.Send(m);
+                                        }
+                                    }
+                                }
+                                catch (Exception x)
+                                {
+                                    Log.Write(x);
+                                }
+                            }
+                        }
+
+                    }
+                }
+                catch (Exception e)
+                {
+                    Log.Write(e);
+                }
+                GC.Collect();
+                GC.GetTotalMemory(true);                
+                await Task.Delay(1000*60*15, stoppingToken);
+                //await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
